f12b0ef1c77906ac256dafaeec2f04805cbbcdf7
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-26 20:55:04 -0300

    salvar produto

--- src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +20,23 @@ public class ProdutoController {
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
+	// busca todos os produtos
 	@GetMapping("/produtos")
 	public List<Produto> listaProdutos() {
 		return produtoRepository.findAll();
 	}
 	
+	// busca produto por id
 	// {id} argumento enviado na url
 	@GetMapping("/produto/{id}")
 	public Produto listaProdutoUnico(@PathVariable(value = "id") long id) {
 		return produtoRepository.findById(id);
 	}
+	
+	// salva produto
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
 }
