9f6f6f93f80d5e40327807f9c84ef7b1892bbc28
Carlos Henrique <ch.ferreir@hotmail.com>
2020-04-26 21:09:32 -0300

    atualiza produto

--- src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -3,9 +3,11 @@ package com.produtos.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,18 @@ public class ProdutoController {
 	// salva produto
 	@PostMapping("/produto")
 	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	// apaga produto
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+	// atualiza produto
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
